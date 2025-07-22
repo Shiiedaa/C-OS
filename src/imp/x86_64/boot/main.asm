81ed62b50ba9ab572263188ccb6be578f79383a8
Shiiedaa <zakeriali1zp@gmail.com>
2025-07-22 15:54:46 -0400

    main.asm update: manually allocated stack

--- src/imp/x86_64/boot/main.asm
+++ src/imp/x86_64/boot/main.asm
@@ -4,8 +4,17 @@ section .text
 bits 32
 
 start:
+
+    mov esp, stack_top ;set up the stack pointer to top of 16KB stack
+
     ; print 'ok'
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
 
 
+ ;define a stack by reserving 16KB of uninitialized space
+ 
+section .bss ;includes statically allocated variables
+stack_bottom:
+    resb 4096 * 4 
+stack_top: 
