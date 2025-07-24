52e9b9cd49bc4a42e4d919d8c3a8943da89d18b9
Shiiedaa <zakeriali1zp@gmail.com>
2025-07-23 21:45:15 -0400

    main long mode check update

--- src/imp/x86_64/boot/main.asm
+++ src/imp/x86_64/boot/main.asm
@@ -45,7 +45,20 @@ no_cpuid:
 
 check_long_mode:
     mov eax, 0x80000000
-    cpu
+    cpuid 
+    cmp eax, 0x80000001
+    jb .no_long_mode
+
+    mov eax, 0x80000001
+    cpuid
+    test edx, 1 << 29 ;lm bit
+    jz .no_long_mode
+
+    ret 
+
+.no_long_mode:
+    mov al, "L"
+    jmp error
 
 ; writes "ERROR: " plus the specific code 
 error:
