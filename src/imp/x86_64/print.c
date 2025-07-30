375991f2e8529744a3055cafafe3fbe1fbaa415d
Shiiedaa <zakeriali1zp@gmail.com>
2025-07-29 21:56:15 -0400

    added print_clear, clear_row

--- src/imp/x86_64/print.c
+++ src/imp/x86_64/print.c
@@ -1,5 +1,8 @@
 #include "print.h"
 
+const static size_t NUM_COLS = 80;
+const static size_t NUM_ROWS = 25;
+
 struct Char
 {
     uint8_t character;
@@ -10,3 +13,19 @@ struct Char *buffer = (struct Char *)0xb8000;
 size_t col = 0;
 size_t row = 0;
 uint8_t color = COLOR_WHITE | COLOR_BLACK << 4;
+
+void clear_row(size_t row)
+{
+    struct Char empty = (struct Char){
+        character : ' ',
+        color : color,
+    };
+}
+
+void print_clear()
+{
+    for (size_t i = 0; i < NUM_ROWS; i++)
+    {
+        clear_row(i);
+    }
+}
